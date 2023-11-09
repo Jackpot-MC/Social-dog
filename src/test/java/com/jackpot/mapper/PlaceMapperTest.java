e8c5f50feb8a6217700f10f6f024169d4c412fcb
Boheon Park <gjsdldi23@naver.com>
2023-11-09 14:44:15 +0900

    place, place type mapper test 추가

--- src/test/java/com/jackpot/mapper/PlaceMapperTest.java
+++ src/test/java/com/jackpot/mapper/PlaceMapperTest.java
@@ -15,26 +15,64 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes= {RootConfig.class})
+@ContextConfiguration(classes = { RootConfig.class })
 @Log4j
+@WebAppConfiguration
 public class PlaceMapperTest {
 
     @Autowired
     PlaceMapper placeMapper;
 
     @Test
-    public void get(Long placeId){
-        System.out.println(1);
-        PlaceVO placeVO = placeMapper.get(1L);
-        log.info(placeVO);
+    public void create() {
+        PlaceVO place = new PlaceVO();
+
+        place.setPlaceId(1L);
+        place.setPlaceName("new name");
+        place.setPlaceSpot("new spot");
+        place.setPlaceTypeId(1L);
+
+        placeMapper.create(place);
     }
 
     @Test
-    public void getList() throws Exception {
+    public void get() {
+        PlaceVO place = placeMapper.get(1L);
+        log.info(place);
+    }
+
+    @Test
+    public void update() {
+        PlaceVO place = placeMapper.get(1L);
+        place.setPlaceSpot("수정된 spot");
+        place.setPlaceName("updated name");
+        place.setPlaceTypeId(2L);
+
+        log.info(place);
+        placeMapper.update(place);
+    }
+
+    @Test
+    public void delete() {
+        placeMapper.delete(1L);
+    }
+
+    @Test
+    public void getList() {
         List<PlaceVO> places = placeMapper.getList();
-        for(PlaceVO p : places){
-            log.info(p);
+        for(PlaceVO place : places){
+            log.info(place);
         }
+    }
+
+    @Test
+    public void insertSelectKey() {
+        PlaceVO place = new PlaceVO();
+        place.setPlaceTypeId(2L);
+        place.setPlaceSpot("new spot2");
+        place.setPlaceName("new name2");
+        log.info("새 객체 : "+place);
+
+        placeMapper.insertSelectKey(place);
     }
 }
