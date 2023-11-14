fd556a9fa4c1cb9c0a4533e2d1cba3c4390ca51f
Younghun Kim <arcticPeng93@gmail.com>
2023-11-14 10:32:33 +0900

    feat: Dog CRUD 작성 및 테스트 완료, Review CRUD 작성 및 테스트 완료.

--- src/test/java/com/jackpot/mapper/ReviewMapperTest.java
+++ src/test/java/com/jackpot/mapper/ReviewMapperTest.java
@@ -1,6 +1,5 @@
 package com.jackpot.mapper;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -8,7 +7,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.jackpot.config.RootConfig;
-import com.jackpot.domain.ReviewVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -26,65 +24,61 @@ public class ReviewMapperTest {
 //		ReviewVO vo = new ReviewVO();
 //		
 //		SimpleDateFormat date1 = new SimpleDateFormat("yyyy-MM-dd");
-//		Date date = date1.parse("2077-07-07");
+//		Date date = date1.parse("2077-07-08");
 //
 //		System.out.println("dog DB에 데이터 삽입 시작");
 //
-//		
-//		vo.setMemberId(2L);
-//		vo.setPlaceId(1L);
-//		vo.setReviewTitle("리뷰타이틀");
-//		vo.setReviewContent("콘텐츠콘텐츠콘텐츠");
+//		vo.setMemberId(5L);
+//		vo.setPlaceId(6L);
+//		vo.setReviewTitle("리뷰타이틀3");
+//		vo.setReviewContent("콘텐츠콘텐츠콘텐츠3");
 //		vo.setReviewDate(date);
-//		vo.setRating(4.3);
+//		vo.setRating(2.0);
 //			
 //		mapper.create(vo);
 //		log.info(vo);
 //
 //		System.out.println("데이터 삽입 끝!");
 //	}
-//
-	@Test
-	public void testGet() {
-		ReviewVO vo = mapper.get(3L);
-
-		System.out.println("get 시작");
-		
-		log.info(vo);
-		log.warn(vo);
-		
-		System.out.println("get 끝!");
-	}
 
 //	@Test
-//	public void testUpdate() {
-//		DogVO dog = mapper.get(4L);
+//	public void testGet() {
+//		ReviewVO vo = mapper.get(5L);
 //
-//		System.out.println("11111111111111111111111111111111");
-//		log.info(dog);
+//		System.out.println("get 시작");
+//		
+//		log.info(vo);
+//		log.warn(vo);
+//		
+//		System.out.println("get 끝!");
+//	}
+
+//	@Test
+//	public void testUpdate() throws Exception {
+//		ReviewVO vo = mapper.get(4L);
+//
+//		log.info(vo);
 //		
 //		SimpleDateFormat date1 = new SimpleDateFormat("yyyy-MM-dd");
-//		Date date = date1.parse("2000-04-01");
+//		Date date = date1.parse("1993-03-05");
 //		
 //		System.out.println("update 시작");
 //		
-//		dog.setDogName("수정멍1");
-//		dog.setDogKind("수정개");
-//		dog.setDogBirth(date);
-//		dog.setDogDescription("꼬리 치며 반갑다고 멍멍멍");
-//		dog.setDogFavorite("Update 테스트 전송");
-//		dog.setDogPhotoPath("c:/temp/temp3.jpg");
+//		vo.setReviewTitle("수정된 리뷰타이틀");
+//		vo.setReviewContent("수정된 콘텐츠콘텐츠콘텐츠");
+//		vo.setReviewDate(date);
+//		vo.setRating(1.0);
 //		
-//		log.info(dog);
+//		log.info(vo);
 //				
-//		mapper.update(dog);
+//		mapper.update(vo);
 //		
 //		System.out.println("update 끝!!!");
 //	}
 
 //	@Test
 //	public void testDelete() {
-//      log.info("DELETE: " + mapper.delete(2L));
+//      log.info("DELETE: " + mapper.delete(6L));
 //	}
 
 //	@Test
