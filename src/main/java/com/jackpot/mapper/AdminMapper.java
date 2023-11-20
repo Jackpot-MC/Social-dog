0b39603ae0300153421c3b212bcb79e08bd3d3c8
Boheon Park <gjsdldi23@naver.com>
2023-11-20 14:06:11 +0900

    feature-admin-login 병합

--- src/main/java/com/jackpot/mapper/AdminMapper.java
+++ src/main/java/com/jackpot/mapper/AdminMapper.java
@@ -1,10 +1,13 @@
 package com.jackpot.mapper;
 
 import com.jackpot.domain.AdminVO;
+import org.apache.ibatis.annotations.Mapper;
 
+
+@Mapper
 public interface AdminMapper {
 	
-	public AdminVO get(Long adminId); // PK 전달
+	public AdminVO get(String adminLoginId); // 로그인 아이디 전달
 	
 	public void insert(AdminVO admin);
 	
@@ -12,5 +15,7 @@ public interface AdminMapper {
 	
 	public int delete(Long adminId); // PK 전달
 	
-	public int deleteLoginId(String adminLoginId); // PK 전달
+	public int deleteLoginId(String adminLoginId); // 로그인 아이디 전달
+
+	public AdminVO login(AdminVO adminVO);//로그인
 }
